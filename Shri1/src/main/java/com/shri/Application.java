9ca6aa0d0def5504c856465bcd0b36e3d3be7cc6
Shri54726 <shri.korat@gmail.com>
2019-12-30 17:07:49 +0530

    My Commit

---
+++ Shri1/src/main/java/com/shri/Application.java
@@ -0,0 +1,11 @@
+package com.shri;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Shrikant");
+		System.out.println("Kasa aahes tu...?");
+
+	}
+
+}
